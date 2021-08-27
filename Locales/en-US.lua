09bf95bfe8d38a21f575cd40e431ac6a0848b2d4
David Moore <david@robokiwi.com>
2021-08-27 22:20:06 +1200

    Text message, new sound options, configuration

---
+++ Locales/en-US.lua
@@ -0,0 +1,34 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("BronCena", "enUS", true)
+
+-- Slash commands
+L["broncena"] = true
+L["bron"] = true
+
+-- Options
+L["Disabled"] = true
+L["Disables or enables the addon"] = true
+L["Zones"] = true
+
+L["World"] = true
+L["Enables or disables when you're not in an instance"] = true
+
+L["Scenario"] = true
+L["Enables or disables when you're in a scenario"] = true
+
+L["Party"] = true
+L["Dungeon"] = true
+L["Raid"] = true
+L["PvP"] = true
+L["Arena"] = true
+L["Owners"] = true
+
+L["Player"] = true
+L["Group"] = true
+L["Friendly"] = true
+L["Enemy"] = true
+
+L["Unlock"] = true;
+L["Enables or disables moving UI"] = true
+
+L["Debug"] = true
+L["Enables or disables debug logging"] = true
